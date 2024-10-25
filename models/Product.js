9c85b2e480058423e7f8311e594b4f68b718ff99
Kariuki James <nyamuj59@gmail.com>
2024-10-25 11:32:54 +0300

    added aggregate pipeline for reviews

--- models/Product.js
+++ models/Product.js
@@ -42,7 +42,7 @@ const ProductSchema = new mongoose.Schema(
         message: '{VALUE} is not supported',
       },
     },
-    color: {
+    colors: {
       type: [String],
       required: true,
     },
@@ -63,6 +63,10 @@ const ProductSchema = new mongoose.Schema(
       type: Number,
       default: 0,
     },
+    numOfReviews: {
+      type: Number,
+      default: 0,
+    },
     user: {
       type: mongoose.Types.ObjectId,
       ref: 'User',
@@ -79,11 +83,10 @@ ProductSchema.virtual('reviews', {
   justOne: false,
 })
 
-ProductSchema.pre('deleteOne', async function (next) {
+ProductSchema.pre('deleteOne', { document: true }, async function (next) {
   try {
-    const productId = this.getQuery()._id
     const Review = mongoose.model('Review')
-    await Review.deleteMany({ product: productId })
+    await Review.deleteMany({ product: this._id })
     next()
   } catch (error) {
     next(error)
--- models/Review.js
+++ models/Review.js
@@ -32,4 +32,37 @@ const ReviewSchema = new mongoose.Schema(
 
 ReviewSchema.index({ product: 1, user: 1 }, { unique: true })
 
+ReviewSchema.statics.calculateAverageRating = async function (productId) {
+  const result = await this.aggregate([
+    { $match: { product: productId } },
+    {
+      $group: {
+        _id: null,
+        averageRating: { $avg: '$rating' },
+        numOfReviews: { $sum: 1 },
+      },
+    },
+  ])
+  console.log(result)
+  try {
+    await this.model('Product').findOneAndUpdate(
+      { _id: productId },
+      {
+        averageRating: Math.ceil(result[0]?.averageRating || 0),
+        numOfReviews: result[0]?.numOfReviews || 0,
+      }
+    )
+  } catch (error) {
+    console.log(error)
+  }
+}
+
+ReviewSchema.post('save', async function () {
+  await this.constructor.calculateAverageRating(this.product)
+})
+
+ReviewSchema.post('deleteOne', { document: true }, async function () {
+  await this.constructor.calculateAverageRating(this.product)
+})
+
 module.exports = mongoose.model('Review', ReviewSchema)
