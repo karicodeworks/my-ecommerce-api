4fa5f173f16e908538e094e1229c6289f829c6e3
Kariuki James <nyamuj59@gmail.com>
2024-10-27 16:22:27 +0300

    edited some doc files

--- app.js
+++ app.js
@@ -56,17 +56,6 @@ app.use(express.json())
 app.use(cookieParser(process.env.JWT_SECRET))
 app.use(fileUpload({ useTempFiles: true }))
 
-app.get('/', (req, res) => {
-  res.send('This is the fist ecommerce page')
-})
-
-app.get('/api/v1', (req, res) => {
-  //console.log(res.cookies)
-  console.log(req.signedCookies)
-
-  res.send('This is the fist ecommerce page')
-})
-
 app.use('/api/v1/auth', authRouter)
 app.use('/api/v1/users', userRouter)
 app.use('/api/v1/products', productRouter)
